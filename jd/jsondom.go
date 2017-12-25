949d6245c8cc64ed96d19eeec4716eb0d6849e92
libbylg <libbylg@126.com>
2017-12-25 08:00:15 +0800

    初步完成json锚点工具开发

---
+++ jd/jsondom.go
@@ -0,0 +1,165 @@
+// Package jd  实现了一个简单的 json 的 DOM 树构造工具.
+package jd
+
+import (
+	"encoding/json"
+)
+
+const (
+	NULL   = 0
+	STRING = 1
+	BOOL   = 2
+	NUMBER = 3
+	MAP    = 4
+	ARRAY  = 5
+)
+
+type Anchor struct {
+	Ref interface{}
+}
+
+func New(data []byte) (Anchor, error) {
+	var v interface{}
+	err := json.Unmarshal(data, &v)
+	if nil != err {
+		return Anchor{nil}, err
+	}
+
+	return Anchor{v}, nil
+}
+
+func (c Anchor) Unmarshal(obj interface{}) error {
+	bytes, err := json.Marshal(c.Ref)
+	if nil != err {
+		return err
+	}
+
+	return json.Unmarshal(bytes, obj)
+}
+
+func (c Anchor) Marshal() ([]byte, error) {
+	return json.Marshal(c.Ref)
+}
+
+func (c Anchor) Type() int {
+	switch c.Ref.(type) {
+	case json.Number:
+		return NUMBER
+	case string:
+		return STRING
+	case bool:
+		return BOOL
+	case map[string]interface{}:
+		return MAP
+	case []interface{}:
+		return ARRAY
+	default:
+		return NULL
+	}
+}
+
+func (c Anchor) String(def string) string {
+	if nil == c.Ref {
+		return def
+	}
+
+	v, ok := c.Ref.(string)
+	if !ok {
+		return def
+	}
+	return v
+}
+
+func (c Anchor) Bool(def bool) bool {
+	if nil == c.Ref {
+		return def
+	}
+
+	v, ok := c.Ref.(bool)
+	if !ok {
+		return def
+	}
+	return v
+}
+
+func (c Anchor) Int(def int64) int64 {
+	if nil == c.Ref {
+		return def
+	}
+
+	v, ok := c.Ref.(json.Number)
+	if !ok {
+		return def
+	}
+
+	t, err := v.Int64()
+	if nil != err {
+		return def
+	}
+
+	return t
+}
+
+func (c Anchor) Float(def float64) float64 {
+	if nil == c.Ref {
+		return def
+	}
+
+	v, ok := c.Ref.(json.Number)
+	if !ok {
+		return 0
+	}
+
+	t, err := v.Float64()
+	if nil != err {
+		return 0
+	}
+
+	return t
+}
+
+func (c Anchor) Map() map[string]interface{} {
+	v, ok := c.Ref.(map[string]interface{})
+	if !ok {
+		return nil
+	}
+
+	return v
+}
+
+func (c Anchor) Array() []interface{} {
+	v, ok := c.Ref.([]interface{})
+	if !ok {
+		return nil
+	}
+
+	return v
+}
+
+func (c Anchor) Quote(key string) Anchor {
+	m, ok := c.Ref.(map[string]interface{})
+	if !ok {
+		return Anchor{nil} //	如果无法转换成 map
+	}
+
+	v, exist := m[key]
+	if !exist {
+		return Anchor{nil}
+	}
+
+	return Anchor{v}
+}
+
+func (c Anchor) Index(index int) Anchor {
+	m, ok := c.Ref.([]interface{})
+	if !ok {
+		return Anchor{nil} //	如果无法转换成 Array
+	}
+
+	if index < 0 || len(m) <= index {
+		c.Ref = nil
+		return Anchor{nil}
+	}
+
+	return Anchor{m[index]}
+}
---
+++ jd/jsondom_test.go
@@ -0,0 +1,38 @@
+package jd
+
+import (
+	"testing"
+)
+
+func expect(t *testing.T, msg string, result bool) {
+	if result {
+		return
+	}
+
+	t.Error(msg)
+}
+
+func Test_1(t *testing.T) {
+	s := `{
+		"key1": [1, "string", null, true, false, {"a":"b","c":"d"}],
+		"key2": {"m1":"sss", "m2":22, "m3":null, "m4":true, "m5":false}
+	}
+	`
+
+	c, err := New([]byte(s))
+	expect(t, "转换应该成功", nil == err)
+
+	v := c.Quote("key1").Index(5).Quote("c").String("")
+	expect(t, "通过链式引用成功", v == "d")
+}
+
+func Test_2(t *testing.T) {
+	s := `{"key1":"value1","key2":22}`
+
+	c, err := New([]byte(s))
+	expect(t, "转换应该成功", nil == err)
+
+	v := c.Map()
+	expect(t, "通过链式引用成功", v["key1"].(string) == "d")
+	//expect(t, "通过链式引用成功", v["key1"].json.N == )
+}
